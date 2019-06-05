fc2d9070ef3ac383f4427b0e792606951f63893c
Stephen <stepheku@users.noreply.github.com>
2019-06-04 21:39:10 -0400

    Added line to set global git editor to vim

--- git_config.sh
+++ git_config.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
 # set up github identity
 git config --global user.email "stepheku@users.noreply.github.com"
 git config --global user.name "stepheku"
+git config --global core.editor "vim"
