335081b193f31f5c7669b8910b5c25ac47bf0715
Stephen <stepheku@users.noreply.github.com>
2020-02-03 10:28:27 -0800

    added shell-launcher extension
    added shell-launcher extension, removed duplicate vscode-remote extensions in favor of extension pack

--- vscode/install-extensions.sh
+++ vscode/install-extensions.sh
@@ -4,14 +4,10 @@ code --install-extension andyyaldoo.vscode-json
 code --install-extension DotJoshJohnson.xml
 code --install-extension eamodio.gitlens
 code --install-extension ms-python.python
-code --install-extension ms-vscode-remote.remote-containers
-code --install-extension ms-vscode-remote.remote-ssh
-code --install-extension ms-vscode-remote.remote-ssh-edit
-code --install-extension ms-vscode-remote.remote-ssh-explorer
-code --install-extension ms-vscode-remote.remote-wsl
 code --install-extension ms-vscode-remote.vscode-remote-extensionpack
 code --install-extension ritwickdey.LiveServer
 code --install-extension slevesque.vscode-autohotkey
 code --install-extension Justin J. Clark.ccl
 code --install-extension vscodevim.vim
 code --install-extension alefragnani.Bookmarks
+code --install-extension Tyriar.shell-launcher
